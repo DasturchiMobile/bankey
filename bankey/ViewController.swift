9ae7ce6abd9e492e9200d0f6e35021a5e5331dda
AzizbekAsqaraliyev <DasturchiMobile@gmail.com>
2024-03-09 00:20:02 +0500

    Initial Commit

---
+++ bankey/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  bankey
+//
+//  Created by Azizbek Asqaraliyev on 09/03/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
